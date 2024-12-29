b054106f3cb39828f5dac7bb9d59b6f04ae459ef
seobbang <seobbangway@naver.com>
2024-12-29 17:02:54 +0900

    feat: react query dev tools 추가 및 globalLayout -> App 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import "@radix-ui/themes/styles.css";
 import Router from "@/Router";
 import { GlobalStyles } from "@/common/styles/GlobalStyles";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Theme } from "@radix-ui/themes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
-      <Router />
-    </>
+      <Theme>
+        <Router />
+      </Theme>
+    </QueryClientProvider>
   );
 }
 
--- src/common/components/GlobalLayout.tsx
+++ src/common/components/GlobalLayout.tsx
@@ -1,15 +1,9 @@
-import { Theme } from "@radix-ui/themes";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Outlet } from "react-router";
-
-const queryClient = new QueryClient();
 
 export default function GlobalLayout() {
   return (
-    <Theme>
-      <QueryClientProvider client={queryClient}>
-        <Outlet />
-      </QueryClientProvider>
-    </Theme>
+    <>
+      <Outlet />
+    </>
   );
 }
