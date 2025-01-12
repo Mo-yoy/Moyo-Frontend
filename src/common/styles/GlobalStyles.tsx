1b449cb09ebc28137cc8e73eb94627c4fe689eb7
jungwoo3490 <jungwoo3490@naver.com>
2025-01-12 19:01:00 +0900

    feat: add Pretendard font-face and update Radix themes default font

--- src/common/styles/GlobalStyles.tsx
+++ src/common/styles/GlobalStyles.tsx
@@ -4,7 +4,19 @@ import type { ReactElement } from "react";
 
 const globalCss = css`
   ${reset}
+
+  @font-face {
+    font-family: "Pretendard";
+    font-weight: 100 900;
+    font-display: swap;
+    src: url("/fonts/PretendardVariable.woff2") format("woff2-variations");
+  }
+
+  .radix-themes {
+    --default-font-family: "Pretendard";
+  }
 `;
+
 export function GlobalStyles(): ReactElement {
   return <Global styles={[globalCss]} />;
 }
