b0eec668e1b2a86b930b1eed06c1957b76680129
seobbang <seobbangway@naver.com>
2025-01-21 01:20:21 +0900

    refaactor: intersection -> extends

--- src/common/components/UserListItemBox.tsx
+++ src/common/components/UserListItemBox.tsx
@@ -1,11 +1,9 @@
 import { Card, type CardProps } from "@radix-ui/themes";
 import type { ReactNode } from "react";
 
-interface UserBoxProps {
+interface Props extends CardProps {
   children: ReactNode;
 }
-
-type Props = UserBoxProps & CardProps;
 
 export default function UserListItemBox({ children, ...props }: Props) {
   return (
