bec9d8c47124518cf19169fa335a8b8ad81f510e
seobbang <seobbangway@naver.com>
2025-03-02 23:36:04 +0900

    feat: max width 800px 설정

--- src/common/components/GlobalLayout.tsx
+++ src/common/components/GlobalLayout.tsx
@@ -1,9 +1,15 @@
+import { rem } from "@/common/utils/rem";
+import styled from "@emotion/styled";
 import { Outlet } from "react-router";
 
 export default function GlobalLayout() {
   return (
-    <>
+    <LayoutWrapper>
       <Outlet />
-    </>
+    </LayoutWrapper>
   );
 }
+
+const LayoutWrapper = styled.div({
+  maxWidth: rem(80),
+});
