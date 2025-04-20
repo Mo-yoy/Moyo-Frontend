cdecc93a8da1d7fc7045cf2b2e11218707b2a7ca
seobbang <seobbangway@naver.com>
2025-04-20 18:33:51 +0900

    feat: localhost https로 띄우기

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
+import fs from "node:fs";
 
 export default defineConfig({
   plugins: [
@@ -11,4 +12,10 @@ export default defineConfig({
     tsconfigPaths(),
     svgr({ include: "**/*.svg?react" }),
   ],
+  server: {
+    https: {
+      key: fs.readFileSync("localhost-key.pem"),
+      cert: fs.readFileSync("localhost.pem"),
+    },
+  },
 });
