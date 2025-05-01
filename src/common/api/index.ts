564579bbe2ffaed222cd10c814f894532176266a
seobbang <seobbangway@naver.com>
2025-05-01 23:34:15 +0900

    feat: create apiClient

---
+++ src/common/api/index.ts
@@ -0,0 +1,37 @@
+import { reissue } from "@/common/api/reissue";
+import { accessTokenStore } from "@/common/stores/accessTokenStore";
+import ky from "ky";
+
+export const apiClient = ky.create({
+  prefixUrl: import.meta.env.VITE_API_BASE_URL,
+  credentials: "include",
+  hooks: {
+    beforeRequest: [
+      (request) => {
+        const atk = accessTokenStore.get();
+
+        if (atk != null) {
+          request.headers.set("Authorization", `Bearer ${atk}`);
+        }
+      },
+    ],
+    afterResponse: [
+      async (request, _options, response) => {
+        if (response.status === 401) {
+          try {
+            const {
+              data: { accessToken },
+            } = await reissue();
+            accessTokenStore.set(accessToken);
+
+            return apiClient(request);
+          } catch (e) {
+            accessTokenStore.clear();
+            window.location.href = "/login";
+          }
+        }
+        return response;
+      },
+    ],
+  },
+});
