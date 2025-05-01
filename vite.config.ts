c789ab81dac826cda8c784b1522bae71124a2991
seobbang <seobbangway@naver.com>
2025-05-01 20:26:20 +0900

    feat: 개발 서버 https 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
 import fs from "node:fs";
 
+const LOCALHOST_KEY_PATH = "localhost-key.pem";
+const LOCALHOST_CERT_PATH = "localhost.pem";
+
+const hasHttpsCertificates = fs.existsSync(LOCALHOST_KEY_PATH) && fs.existsSync(LOCALHOST_CERT_PATH);
+
 export default defineConfig({
   plugins: [
     react({
@@ -13,9 +18,14 @@ export default defineConfig({
     svgr({ include: "**/*.svg?react" }),
   ],
   server: {
-    https: {
-      key: fs.readFileSync("localhost-key.pem"),
-      cert: fs.readFileSync("localhost.pem"),
-    },
+    /** 서비스 로직에서 reissue가 필수적이므로 클라이언트에 refresh token을 쿠키로 설정할 수 있어야 합니다.
+     * 이를 위해 API dev 서버는 인증 과정에서 https로 리다이렉션을 설정하고, 클라이언트 개발 환경도 https 서버로 구성합니다.
+     */
+    https: hasHttpsCertificates
+      ? {
+          key: fs.readFileSync(LOCALHOST_KEY_PATH),
+          cert: fs.readFileSync(LOCALHOST_CERT_PATH),
+        }
+      : undefined,
   },
 });
