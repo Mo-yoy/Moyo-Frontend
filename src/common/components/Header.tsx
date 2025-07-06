a9962c58da3074a8d7002e3834730942d12a469a
Yeonseo-Jo <jys073190@gmail.com>
2025-07-06 17:37:24 +0900

    fix: header 컴포넌트 인터페이스 추가

--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -1,19 +1,61 @@
 import styled from "@emotion/styled";
+import { ChevronLeftIcon } from "@radix-ui/react-icons";
 import type { ReactElement } from "react";
+import { useNavigate } from "react-router";
 import { Z_INDEX } from "../constants/zIndex";
+import { rem } from "../utils/rem";
 
 interface HeaderProps {
-  left?: ReactElement | string;
-  center: ReactElement | string;
-  right?: ReactElement | string;
+  backButton?: {
+    renderIcon?: () => ReactElement;
+    onClick?: () => void;
+  };
+  title?: string;
+  renderLeft?: () => ReactElement;
+  renderCenter?: () => ReactElement;
+  renderRight?: () => ReactElement;
 }
 
-function Header({ left, center, right }: HeaderProps) {
+function Header({ backButton, title, renderLeft, renderCenter, renderRight }: HeaderProps) {
+  const navigate = useNavigate();
+
+  const renderBackButton = () => {
+    const onClickBackButton = () => {
+      if (backButton && "onClick" in backButton && backButton.onClick) {
+        backButton.onClick();
+        return;
+      }
+      // default behavior
+      navigate(-1);
+    };
+
+    return (
+      <button type="button" onClick={onClickBackButton}>
+        {(() => {
+          if (backButton && "renderIcon" in backButton && backButton.renderIcon) {
+            return backButton.renderIcon();
+          }
+          // default back button icon
+          return <ChevronLeftIcon width={28} height={28} />;
+        })()}
+      </button>
+    );
+  };
+
+  const renderCentnerElement = () => renderCenter && <CenterElement as="div">{renderCenter()}</CenterElement>;
+
+  const renderTitleElement = () =>
+    title && (
+      <CenterElement as="h1" css={{ fontSize: rem(1.8), fontWeight: "700" }}>
+        {title}
+      </CenterElement>
+    );
+
   return (
     <HeaderWrapper aria-label="페이지 헤더">
-      <LeftElement>{left}</LeftElement>
-      <CenterElement as={typeof center === "string" ? "h1" : "div"}>{center}</CenterElement>
-      <RightElement>{right}</RightElement>
+      <LeftElement>{renderLeft?.() ?? renderBackButton()}</LeftElement>
+      {renderCentnerElement() || renderTitleElement()}
+      <RightElement>{renderRight?.()}</RightElement>
     </HeaderWrapper>
   );
 }
@@ -31,6 +73,7 @@ const HeaderWrapper = styled.header({
   height: "4.8rem",
 
   backgroundColor: "white",
+  boxShadow: "0 2px 4px 0 rgba(0, 0, 0, 0.1)",
 });
 
 const LeftElement = styled.div({
