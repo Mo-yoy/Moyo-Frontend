29818bf3c2dc5e839d1bcb9027f49e9b257298b6
seobbang <seobbangway@naver.com>
2025-08-18 00:04:21 +0900

    feat: 팔로우 언팔로우 버튼 구현

--- src/Follow/components/FollowList.tsx
+++ src/Follow/components/FollowList.tsx
@@ -1,14 +1,20 @@
-import type { FollowDetectResponse } from "@/Follow/apis/follow";
+import type { FollowDetectResponse, FollowDetectType } from "@/Follow/apis/follow";
+import { useCreateFollowUser } from "@/Follow/hooks/apis/useCreateFollowUser";
+import { useDeleteUnfollowUser } from "@/Follow/hooks/apis/useDeleteUnfollowUser";
 import UserListItemCard from "@/common/components/UserListItemCard";
 import UserProfileAvatar from "@/common/components/UserProfileAvatar";
 import { Button, Flex } from "@radix-ui/themes";
 import { Text } from "@radix-ui/themes";
 
 interface FollowListProps {
+  type: FollowDetectType;
   userList: FollowDetectResponse["userList"];
 }
 
-export function FollowList({ userList }: FollowListProps) {
+export function FollowList({ type, userList }: FollowListProps) {
+  const { mutateAsync: unfollow } = useDeleteUnfollowUser();
+  const { mutateAsync: follow } = useCreateFollowUser();
+
   return (
     <Flex direction="column" gap="2" css={{ padding: "0 2rem" }}>
       {userList.map((user) => (
@@ -22,9 +28,37 @@ export function FollowList({ userList }: FollowListProps) {
               </Text>
             </Flex>
             <Flex gap="3">
-              <Button color="indigo" variant="soft" size="1">
-                Unfollow
-              </Button>
+              {type === "followed-only" ? (
+                <Button
+                  color="indigo"
+                  variant="soft"
+                  size="1"
+                  onClick={async () => {
+                    try {
+                      follow(user.githubUserId);
+                    } catch (e) {
+                      alert("팔로우에 실패했습니다. 다시 시도해주세요");
+                    }
+                  }}
+                >
+                  Follow
+                </Button>
+              ) : (
+                <Button
+                  color="indigo"
+                  variant="soft"
+                  size="1"
+                  onClick={async () => {
+                    try {
+                      unfollow(user.githubUserId);
+                    } catch (e) {
+                      alert("언팔로우에 실패했습니다. 다시 시도해주세요");
+                    }
+                  }}
+                >
+                  Unfollow
+                </Button>
+              )}
             </Flex>
           </Flex>
         </UserListItemCard>
