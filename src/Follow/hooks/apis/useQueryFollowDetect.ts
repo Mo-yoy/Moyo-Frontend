fdc0f5fb23dc33a61303c86e034838728fc055da
seobbang <seobbangway@naver.com>
2025-08-29 01:48:26 +0900

    feat: 무한스크롤 구현

--- src/Follow/hooks/apis/useQueryFollowDetect.ts
+++ src/Follow/hooks/apis/useQueryFollowDetect.ts
@@ -1,10 +1,26 @@
-import { type FollowDetectType, queryFollowDetect } from "@/Follow/apis/follow";
+import { type FollowDetectResponse, type FollowDetectType, queryFollowDetect } from "@/Follow/apis/follow";
 import { followQueryKeys } from "@/Follow/hooks/apis/queryKeys";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import type { ApiResponse } from "@/common/types/api";
+import { type InfiniteData, useInfiniteQuery } from "@tanstack/react-query";
 
 export function useQueryFollowDetect(detectType: FollowDetectType) {
-  return useSuspenseQuery({
+  return useInfiniteQuery<
+    ApiResponse<FollowDetectResponse>,
+    Error,
+    InfiniteData<ApiResponse<FollowDetectResponse>>,
+    string[],
+    number | undefined
+  >({
     queryKey: followQueryKeys.list(detectType),
-    queryFn: () => queryFollowDetect(detectType),
+    queryFn: ({ pageParam }) => queryFollowDetect(detectType, pageParam),
+    initialPageParam: undefined,
+    getNextPageParam: (prevPage) => {
+      const lastPage = prevPage.data.lastPage;
+      if (lastPage) return undefined;
+
+      const userList = prevPage.data.userList;
+      return userList[userList.length - 1].githubUserId;
+    },
+    staleTime: 60000,
   });
 }
