a45b8f488dd0da169109bc27268234b3a0dbd805
jungwoo3490 <jungwoo3490@naver.com>
2025-09-17 17:04:45 +0900

    feat: 시상대 아이템 컴포넌트 구현

---
+++ src/Ranking/components/PodiumItem.tsx
@@ -0,0 +1,78 @@
+import UserProfileAvatar from "@/common/components/UserProfileAvatar";
+import { rem } from "@/common/utils/rem";
+import styled from "@emotion/styled";
+
+interface PodiumItemProps {
+  rank: number;
+  nickname: string;
+  point: number;
+  profileImage: string;
+}
+
+export function PodiumItem({ rank, nickname, point, profileImage }: PodiumItemProps) {
+  return (
+    <PodiumItemWrapper>
+      <ImageWrapper>
+        <UserProfileAvatar src={profileImage} alt="프로필 사진" style={{ width: "100%", height: "100%" }} />
+        <Medal rank={rank}>{rank}</Medal>
+      </ImageWrapper>
+      <Nickname rank={rank}>{nickname}</Nickname>
+      <Point>{point}pt</Point>
+    </PodiumItemWrapper>
+  );
+}
+
+const PodiumItemWrapper = styled.div({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+
+  width: "fit-content",
+});
+
+const ImageWrapper = styled.div({
+  position: "relative",
+
+  width: rem(7.8),
+  height: rem(7.8),
+  padding: rem(0.3),
+  backgroundColor: "#ffffff",
+
+  borderRadius: rem(999),
+});
+
+const Medal = styled.div<{ rank: number }>(({ rank }) => ({
+  position: "absolute",
+  bottom: 0,
+  right: 0,
+
+  width: rem(2.4),
+  height: rem(2.4),
+
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+
+  fontSize: rem(1.7),
+  fontWeight: 700,
+  color: "#ffffff",
+
+  borderRadius: rem(999),
+  backgroundColor: rank === 1 ? "#F8A900" : rank === 2 ? "#B4B5BB" : rank === 3 ? "#D46A28" : "#FFFFFF",
+}));
+
+const Nickname = styled.div<{ rank: number }>(({ rank }) => ({
+  marginTop: rem(0.5),
+
+  fontSize: rem(1.7),
+  fontWeight: 700,
+  color: rank === 1 ? "#F8A900" : rank === 2 ? "#B4B5BB" : rank === 3 ? "#D46A28" : "#ffffff",
+}));
+
+const Point = styled.div({
+  marginTop: rem(0.1),
+
+  fontSize: rem(1.7),
+  fontWeight: 700,
+  color: "#ffffff",
+});
