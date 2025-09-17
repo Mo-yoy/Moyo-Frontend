5686395c76dbaa5d52d1078d06a98bc603503df4
jungwoo3490 <jungwoo3490@naver.com>
2025-09-17 18:16:39 +0900

    feat: 랭킹 옵션 버튼 그룹 구현

--- src/Ranking/page.tsx
+++ src/Ranking/page.tsx
@@ -2,14 +2,44 @@ import ImgPodium from "@/assets/images/test.jpg";
 import Header from "@/common/components/Header";
 import { rem } from "@/common/utils/rem";
 import styled from "@emotion/styled";
+import { useState } from "react";
 import { PodiumItem } from "./components/PodiumItem";
 
 export function RankingPage() {
+  const [selectedOption, setSelectedOption] = useState<"weekly" | "monthly" | "yearly">("weekly");
+
   return (
     <>
       <Header title="랭킹" />
       <PageBody>
         <PodiumContainer>
+          <RankingOptionButtonGroup>
+            <RankingOptionButton isSelected={selectedOption === "weekly"} onClick={() => setSelectedOption("weekly")}>
+              주간
+            </RankingOptionButton>
+            <div
+              style={{
+                width: rem(0.1),
+                height: rem(2),
+                margin: `0 ${rem(0.3)}`,
+                backgroundColor: "#A1A1A1",
+              }}
+            />
+            <RankingOptionButton isSelected={selectedOption === "monthly"} onClick={() => setSelectedOption("monthly")}>
+              월간
+            </RankingOptionButton>
+            <div
+              style={{
+                width: rem(0.1),
+                height: rem(2),
+                margin: `0 ${rem(0.3)}`,
+                backgroundColor: "#A1A1A1",
+              }}
+            />
+            <RankingOptionButton isSelected={selectedOption === "yearly"} onClick={() => setSelectedOption("yearly")}>
+              연간
+            </RankingOptionButton>
+          </RankingOptionButtonGroup>
           <PodiumItemWrapper>
             <PodiumItem rank={2} nickname="spongebob" point={1000} profileImage={ImgPodium} />
             <PodiumItem rank={1} nickname="spongebob" point={1000} profileImage={ImgPodium} />
@@ -27,6 +57,10 @@ const PageBody = styled.div`
 `;
 
 const PodiumContainer = styled.div({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+
   width: "100%",
   height: rem(26),
 
@@ -42,3 +76,35 @@ const PodiumItemWrapper = styled.div({
   justifyContent: "center",
   gap: rem(3),
 });
+
+const RankingOptionButtonGroup = styled.div({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+
+  width: rem(22),
+  height: rem(2.7),
+  padding: `0 ${rem(0.3)}`,
+  marginTop: rem(1.2),
+
+  borderRadius: rem(3),
+  backgroundColor: "rgba(75, 75, 75, 0.85)",
+});
+
+const RankingOptionButton = styled.button<{ isSelected: boolean }>(({ isSelected }) => ({
+  flex: 1,
+
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+
+  height: rem(2.1),
+
+  borderRadius: rem(3),
+
+  fontSize: rem(1),
+  fontWeight: 700,
+  color: "#ffffff",
+
+  backgroundColor: isSelected ? "#A1A1A1" : "transparent",
+}));
