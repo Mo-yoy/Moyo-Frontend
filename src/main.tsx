16aff8d58f5cafabda586208fd2bbccb6786d626
jungwoo3490 <jungwoo3490@naver.com>
2025-09-26 05:29:37 +0900

    chore: StrictMode 주석 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import App from "@/App";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 const rootElement = document.getElementById("root");
@@ -7,7 +8,7 @@ if (!rootElement) {
 }
 
 createRoot(rootElement).render(
-  // <StrictMode>
-  <App />
-  // </StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
