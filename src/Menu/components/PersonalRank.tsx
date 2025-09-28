396f70bc4e68f411082c4057520e1f3e498a03dd
seobbang <seobbangway@naver.com>
2025-09-28 23:35:03 +0900

    feat: 개인 랭킹 api 연결

--- src/Menu/components/PersonalRank.tsx
+++ src/Menu/components/PersonalRank.tsx
@@ -1,3 +1,4 @@
+import { useQueryUserRanking } from "@/Ranking/hooks/useQueryUserRanking";
 import UserListItemCard from "@/common/components/UserListItemCard";
 import UserProfileAvatar from "@/common/components/UserProfileAvatar";
 import { colors } from "@/common/styles/theme";
@@ -6,29 +7,34 @@ import styled from "@emotion/styled";
 import { Flex, Text } from "@radix-ui/themes";
 
 export function PersonalRank() {
+  const { data: userRanking } = useQueryUserRanking({ duration: "week" });
+  const allRankingList = userRanking?.pages.flatMap((page) => page.data.rankingList) ?? [];
+
+  const topFive = allRankingList.slice(0, 5);
+
   return (
     <Container direction="column" gap="4">
       <Flex justify="between">
         <Text size="5" weight="bold">
-          👑 개인 랭킹
+          👑 개인 주간 랭킹
         </Text>
         <button type="button">랭킹 더보기</button>
       </Flex>
       <Flex direction="column" gap="2">
-        {[1, 2, 3, 4, 5].map((num) => (
-          <UserListItemCard key={num}>
+        {topFive.map(({ userid, profileImageUrl, username, rankPoint }, idx) => (
+          <UserListItemCard key={userid}>
             <Flex justify="between" align="center" css={{ padding: 2 }}>
               <Flex gap="4" align="center">
                 <Text size="4" weight="medium">
-                  {num}
+                  {idx + 1}
                 </Text>
-                <UserProfileAvatar />
+                <UserProfileAvatar src={profileImageUrl} />
 
                 <Text size="2" weight="medium">
-                  name{" "}
+                  {username}
                 </Text>
               </Flex>
-              <Flex gap="3">pt</Flex>
+              <Flex gap="3">{rankPoint}pt</Flex>
             </Flex>
           </UserListItemCard>
         ))}
